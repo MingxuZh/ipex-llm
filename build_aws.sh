a1d13f5880009ac85c1cb4731028c7b6e665821e
mingxu <mingxu.zhang@intel.com>
2023-08-11 14:19:42 +0800

    aws

---
+++ build_aws.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir /home/ubuntu/workspace
+WORKSPACE=/home/ubuntu/workspace
+set -x
+sudo chmod 777  ${WORKSPACE} -R
+rm -rf ${WORKSPACE}/log/
+lscpu
+
+cd /home/ubuntu/workspace
+
+export http_proxy=http://proxy-dmz.intel.com:912
+export https_proxy=http://proxy-dmz.intel.com:912
+
+git clone https://github.com/MingxuZh/ipex-llm
+cd /home/ubuntu/workspace/ipex-llm
+
+sh /home/ubuntu/workspace/ipex-llm/build_awsllm.sh
+
---
+++ build_awsllm.sh
@@ -0,0 +1,29 @@
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+            #  --build-arg TORCHCCL_WHEEL=oneccl_bind_pt-*.whl  \
+#
+
+set -e
+set -x
+
+docker_name=forllmaws
+
+echo "docker_name: ${docker_name}"
+IMAGE_NAME=${docker_name}
+ipex_branch=main
+NODE_PROXY=http://proxy-dmz.intel.com:912
+
+
+sudo docker build --no-cache \
+             --build-arg http_proxy=$NODE_PROXY \
+             --build-arg https_proxy=$NODE_PROXY \
+             -t $IMAGE_NAME \
+             -f DockerFileaws.llm .
