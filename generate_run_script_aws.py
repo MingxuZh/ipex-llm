b1146de64e8b2f68a19173f020190843fbf50d83
mingxu <mingxu.zhang@intel.com>
2023-08-13 19:21:24 +0800

    opt

--- generate_run_script_aws.py
+++ generate_run_script_aws.py
@@ -141,7 +141,7 @@ def generate_commands(yml_file,mode,extra_kmp):
                 for dtype in data['modelargs'][mode]['dtype']:
                     for input_token in data['modelargs'][mode]['inputtokens']:
                         lines.append(f"nohup bash /root/workspace/ipex-llm/get_mem.sh >> $log_dir/mem-usage-llm_{mode}_{model_id.replace('/','-')}_{dtype}_{input_token}.log 2>&1 || true &")
-                        lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -m 0 -N 0 python {data['modelargs'][mode]['scriptname']} --benchmark --input-tokens {input_token} -m {model_id} --dtype {dtype} --ipex --jit --token-latency \
+                        lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -m 0 -N 0 python {data['modelargs'][mode]['scriptname']} --device {data['modelargs'][mode]['device'][0]} --benchmark --input-tokens {input_token} -m {model_id} --dtype {dtype} --ipex --jit --token-latency \
                                     2>&1 | tee -a $log_dir/llm_{mode}_{model_id.replace('/','-')}_{dtype}_{input_token}.log")
                         lines.append(f"collect_perf_logs_llm llm_{mode}_{model_id.replace('/','-')}_{dtype}_{input_token}.log")
 
@@ -159,7 +159,7 @@ def generate_commands(yml_file,mode,extra_kmp):
 
                     for input_token in data['modelargs'][mode]['inputtokens']:
                         lines.append(f"nohup bash /root/workspace/ipex-llm/get_mem.sh >> $log_dir/mem-usage-llm_{mode}_{model_id.replace('/','-')}_{dtype}_{input_token}.log 2>&1 || true &")
-                        lines.append(f"numactl -m 0 -N 0 python {data['modelargs'][mode]['scriptname']} -m {model_id} --quantized-model-path {data['modelargs'][mode]['bestpath']} --benchmark --input-tokens {input_token} --jit --int8-bf16-mixed --token-latency \
+                        lines.append(f"numactl -m 0 -N 0 python {data['modelargs'][mode]['scriptname']} -m {model_id} --quantized-model-path {data['modelargs'][mode]['bestpath']} --device {data['modelargs'][mode]['device'][0]} --benchmark --input-tokens {input_token} --jit --int8-bf16-mixed --token-latency \
                                     2>&1 | tee -a $log_dir/llm_{mode}_{model_id.replace('/','-')}_{dtype}_{input_token}.log")
                         lines.append(f"collect_perf_logs_llm llm_{mode}_{model_id.replace('/','-')}_{dtype}_{input_token}.log")
 
@@ -177,7 +177,7 @@ def generate_commands(yml_file,mode,extra_kmp):
                         for input_token in data['modelargs'][mode]['inputtokens']:
                         # lines.append(f"python {data['modelargs'][mode]['scriptname']} --ipex-weight-only-quantization --lambada --output-dir {data['modelargs'][mode]['outdir']} --jit --int8 -m {model_id}")
                             lines.append(f"nohup bash /root/workspace/ipex-llm/get_mem.sh >> $log_dir/mem-usage-llm_{mode}_{model_id.replace('/','-')}_{dtype}_{input_token}.log 2>&1 || true &")
-                            lines.append(f"numactl -m 0 -N 0 python {data['modelargs'][mode]['scriptname']} -m {model_id} --quantized-model-path {data['modelargs'][mode]['bestpath']} --benchmark --input-tokens {input_token} --int8 --jit --token-latency \
+                            lines.append(f"numactl -m 0 -N 0 python {data['modelargs'][mode]['scriptname']} -m {model_id} --quantized-model-path {data['modelargs'][mode]['bestpath']} --device {data['modelargs'][mode]['device'][0]} --benchmark --input-tokens {input_token} --int8 --jit --token-latency \
                                     2>&1 | tee -a $log_dir/llm_{mode}_{model_id.replace('/','-')}_{dtype}_{input_token}.log")
                             lines.append(f"collect_perf_logs_llm llm_{mode}_{model_id.replace('/','-')}_{dtype}_{input_token}.log")
 
