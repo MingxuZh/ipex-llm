e079a887318420065a89c0f5f36729ebd67506aa
mingxu <mingxu.zhang@intel.com>
2023-10-23 12:47:53 +0800

    debug

--- runrun.py
+++ runrun.py
@@ -74,6 +74,7 @@ parser.add_argument(
     default="None",
     help="Specify the kv_cache data type, you can use float8_e5m2 to reduce kv_cache memory footprint but may slightly drop the accuracy.",
 )
+parser.add_argument("--prompt", default=None, type=str)
 parser.add_argument("--int8-bf16-mixed", action="store_true", help="int8 mixed bf16")
 parser.add_argument("--quantized-model-path", default="./saved_result/best_model.pt")
 parser.add_argument(
