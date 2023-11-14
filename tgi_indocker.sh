b164f4c4c0d613611d0a10d5a30e115e16893487
mingxu <mingxu.zhang@intel.com>
2023-11-14 20:02:57 +0800

    ;

--- tgi_indocker.sh
+++ tgi_indocker.sh
@@ -13,17 +13,17 @@ pip uninstall -y torch
 pip install torch==2.1
 pip install http://mlpc.intel.com/downloads/cpu/ipex-2.1/ipex_release/rc2/intel_extension_for_pytorch-2.1.0+cpu-cp39-cp39-linux_x86_64.whl
 
-nohup numactl -C 0-55 -m 0 text-generation-launcher -p 80 --model-id huggyllama/llama-7b --dtype bfloat16 2>&1 | tee -a /data/serve.log &
+nohup numactl -C 0-55 -m 0 text-generation-launcher -p 80 --model-id huggyllama/llama-7b --dtype bfloat16 > /data/serve.log &
 
 sleep 480s
 
-curl 127.0.0.1:8088/generate -X POST -d '{"inputs":"What is Deep Learning?","parameters":{"max_new_tokens":128, "do_sample": true}}' -H 'Content-Type: application/json' 2>&1 | tee -a /data/client.log &
+curl 127.0.0.1:8088/generate -X POST -d '{"inputs":"What is Deep Learning?","parameters":{"max_new_tokens":128, "do_sample": true}}' -H 'Content-Type: application/json' > /data/client.log &
 wait 
-curl 127.0.0.1:8088/generate -X POST -d '{"inputs":"What is Deep Learning?","parameters":{"max_new_tokens":128, "do_sample": true}}' -H 'Content-Type: application/json' 2>&1 | tee -a /data/client.log &
+curl 127.0.0.1:8088/generate -X POST -d '{"inputs":"What is Deep Learning?","parameters":{"max_new_tokens":128, "do_sample": true}}' -H 'Content-Type: application/json' > /data/client.log &
 wait 
-curl 127.0.0.1:8088/generate -X POST -d '{"inputs":"What is Deep Learning?","parameters":{"max_new_tokens":128, "do_sample": true}}' -H 'Content-Type: application/json' 2>&1 | tee -a /data/client.log &
+curl 127.0.0.1:8088/generate -X POST -d '{"inputs":"What is Deep Learning?","parameters":{"max_new_tokens":128, "do_sample": true}}' -H 'Content-Type: application/json' > /data/client.log &
 wait 
-curl 127.0.0.1:8088/generate -X POST -d '{"inputs":"What is Deep Learning?","parameters":{"max_new_tokens":128, "do_sample": true}}' -H 'Content-Type: application/json' 2>&1 | tee -a /data/client.log &
+curl 127.0.0.1:8088/generate -X POST -d '{"inputs":"What is Deep Learning?","parameters":{"max_new_tokens":128, "do_sample": true}}' -H 'Content-Type: application/json' > data/client.log &
 wait 
-curl 127.0.0.1:8088/generate -X POST -d '{"inputs":"What is Deep Learning?","parameters":{"max_new_tokens":128, "do_sample": true}}' -H 'Content-Type: application/json' 2>&1 | tee -a /data/client.log &
+curl 127.0.0.1:8088/generate -X POST -d '{"inputs":"What is Deep Learning?","parameters":{"max_new_tokens":128, "do_sample": true}}' -H 'Content-Type: application/json' > /data/client.log &
 wait
