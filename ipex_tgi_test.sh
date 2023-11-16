a60d261c0764112524e89a8069167e9c1c889a90
mingxu <mingxu.zhang@intel.com>
2023-11-16 17:13:53 +0800

    debug

--- ipex_tgi_test.sh
+++ ipex_tgi_test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -ex
 
-cd /bhavanis
-git clone -b main https://github.com/jianan-gu/text-generation-inference
+# cd /bhavanis
+# git clone -b main https://github.com/jianan-gu/text-generation-inference
 cd text-generation-inference
 
-sudo docker build --build-arg http_proxy=http://proxy-chain.intel.com:911 --build-arg https_proxy=http://proxy-chain.intel.com:911 --build-arg no_proxy='*.intel.com' -t tgi_xeon .
-mkdir $PWD/data
+# sudo docker build --build-arg http_proxy=http://proxy-chain.intel.com:911 --build-arg https_proxy=http://proxy-chain.intel.com:911 --build-arg no_proxy='*.intel.com' -t tgi_xeon .
+# mkdir $PWD/data
 export volume=$PWD/data
 
 cd $PWD/data
@@ -19,13 +19,13 @@ sudo docker run -dt --shm-size 1g -p 8088:80 -d --cap-add=sys_nice --ipc=host -v
 sleep 480s
 
 curl 127.0.0.1:8088/generate -X POST -d '{"inputs":"What is Deep Learning?","parameters":{"max_new_tokens":128, "do_sample": false}}' -H 'Content-Type: application/json' >> /data/client.log 2>&1 || true &
-sleep 10s
+sleep 20s
 curl 127.0.0.1:8088/generate -X POST -d '{"inputs":"What is Deep Learning?","parameters":{"max_new_tokens":128, "do_sample": false}}' -H 'Content-Type: application/json' >> /data/client.log 2>&1 || true &
-sleep 10s
+sleep 20s
 curl 127.0.0.1:8088/generate -X POST -d '{"inputs":"What is Deep Learning?","parameters":{"max_new_tokens":128, "do_sample": false}}' -H 'Content-Type: application/json' >> /data/client.log 2>&1 || true &
-sleep 10s
+sleep 20s
 curl 127.0.0.1:8088/generate -X POST -d '{"inputs":"What is Deep Learning?","parameters":{"max_new_tokens":128, "do_sample": false}}' -H 'Content-Type: application/json' >> data/client.log 2>&1 || true &
-sleep 10s
+wait
 curl 127.0.0.1:8088/generate -X POST -d '{"inputs":"What is Deep Learning?","parameters":{"max_new_tokens":128, "do_sample": false}}' -H 'Content-Type: application/json' >> /data/client.log 2>&1 || true &
-sleep 10s
+sleep 20s
 
