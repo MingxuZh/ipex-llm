dc83a32fddf69b61b3ab07eaf690c835c58f232c
MingxuZh <109504044+MingxuZh@users.noreply.github.com>
2023-12-06 11:49:48 +0800

    Update tgi_indocker.sh

--- tgi_indocker.sh
+++ tgi_indocker.sh
@@ -30,5 +30,17 @@ pip install --upgrade huggingface_hub
 huggingface-cli login --token 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
 
 # nohup numactl -C 0-55 -m 0 text-generation-launcher -p 80 --model-id EleutherAI/gpt-j-6B huggyllama/llama-7b --dtype bfloat16 >> /data/serve.log 2>&1 || true &
+export KMP_BLOCKTIME=INF
+export KMP_TPAUSE=0
+export KMP_SETTINGS=1
+export KMP_FORKJOIN_BARRIER_PATTERN=dist,dist
+export KMP_PLAIN_BARRIER_PATTERN=dist,dist
+export KMP_REDUCTION_BARRIER_PATTERN=dist,dist
+
+export LD_PRELOAD=${LD_PRELOAD}:${CONDA_PREFIX}/lib/libtcmalloc.so
+
+export LD_PRELOAD=${LD_PRELOAD}:${CONDA_PREFIX}/lib/libiomp5.so
+
+export LD_PRELOAD=${CONDA_PREFIX}/lib/libstdc++.so.6
 
 nohup numactl -C 0-55 -m 0 text-generation-launcher -p 80 --model-id meta-llama/Llama-2-7b-hf --dtype bfloat16 2>&1 | tee -a /data/serve.log
