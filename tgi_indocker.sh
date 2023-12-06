f9d1527ba7ee7d3c259a7cbfdb223ffec6287567
MingxuZh <109504044+MingxuZh@users.noreply.github.com>
2023-12-06 17:06:44 +0800

    Update tgi_indocker.sh

--- tgi_indocker.sh
+++ tgi_indocker.sh
@@ -22,13 +22,17 @@ cp -r $HOME/.local/lib/libtcmalloc.so ${CONDA_PREFIX}/lib/libtcmalloc.so
 
 
 pip uninstall -y torch
-pip install torch==2.2.0.dev20231007+cpu --index-url https://download.pytorch.org/whl/nightly/cpu
+pip install cmake lit
+pip install https://download.pytorch.org/whl/nightly/cpu/torch-2.2.0.dev20231007%2Bcpu-cp39-cp39-linux_x86_64.whl
+# pip install torch==2.2.0.dev20231007+cpu --index-url https://download.pytorch.org/whl/nightly/cpu
 
-git clone   https://github.com/intel-innersource/frameworks.ai.pytorch.ipex-cpu -b llm_feature_branch
-cd frameworks.ai.pytorch.ipex-cpu
-git submodule update --init --recursive
+# git clone   https://github.com/intel-innersource/frameworks.ai.pytorch.ipex-cpu -b llm_feature_branch
+# cd frameworks.ai.pytorch.ipex-cpu
+# git submodule update --init --recursive
+cd /data/frameworks.ai.pytorch.ipex-cpu
 python setup.py install
 pip install https://download.pytorch.org/whl/nightly/pytorch_triton-2.1.0%2B6e4932cda8-cp39-cp39-linux_x86_64.whl
+cd ..
 
 pip uninstall -y torch # install torch with cuda version to support flash
 pip install https://download.pytorch.org/whl/nightly/cu118/torch-2.2.0.dev20231007%2Bcu118-cp39-cp39-linux_x86_64.whl
