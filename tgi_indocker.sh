2c8b0fc9fcd534b370c7fe297d52597b1c11758a
MingxuZh <109504044+MingxuZh@users.noreply.github.com>
2024-02-23 11:49:57 +0800

    Update tgi_indocker.sh

--- tgi_indocker.sh
+++ tgi_indocker.sh
@@ -48,7 +48,7 @@ export TRANSFORMERS_OFFLINE=0
 export HF_HOME=/root/.cache/huggingface
 # pip install --upgrade huggingface_hub
 pip install huggingface-hub==0.16.4
-huggingface-cli login --token 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
+huggingface-cli login --token 
 
 # nohup numactl -C 0-55 -m 0 text-generation-launcher -p 80 --model-id EleutherAI/gpt-j-6B huggyllama/llama-7b --dtype bfloat16 --max-input-length  4096 --max-total-tokens 8192 >> /data/serve.log 2>&1 || true &
 export KMP_BLOCKTIME=INF
